c03d5e5fb812117414b54c24c74b977c671b2225
bn-khalil <benkhaliltouda3201@gmail.com>
2025-05-17 18:05:10 +0100

    ex05

---
+++ day01/ex05/Harl.hpp
@@ -0,0 +1,16 @@
+#ifndef Harl_H
+#define Harl_H
+
+#include <iostream>
+
+class Harl
+{
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+    public:
+        void complain( std::string level );
+};
+#endif
