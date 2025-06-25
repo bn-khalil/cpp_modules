d1c0ca6d5066c9a5eaef71b5c7511c2f3b6cb14c
bn-khalil <benkhaliltouda3201@gmail.com>
2025-06-25 16:08:44 +0100

    day01

--- day01/ex06/Harl.cpp
+++ day01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: kben-tou <kben-tou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 14:18:44 by kben-tou          #+#    #+#             */
-/*   Updated: 2025/05/18 15:07:34 by kben-tou         ###   ########.fr       */
+/*   Updated: 2025/06/25 16:07:48 by kben-tou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,55 @@
 
 void Harl::debug( void ) {
     std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl\
+    << "I really do!" << std::endl << std::endl;
 }
+
 void Harl::info( void ) {
     std::cout << "[ INFO ]" << std::endl;
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl\
+    << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 }
+
 void Harl::warning( void ) {
     std::cout << "[ WARNING ]" << std::endl;
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free." << std::endl\
+    << "I’ve been coming for years, whereas you started working here just last month." << std::endl << std::endl;
 }
+
 void Harl::error( void ) {
     std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }
+
 void Harl::complain( std::string level ) {
-    std::string tys[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    void ( Harl::*com_s[4] )( void ) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-    int choice = -1;
-    for (int i = 0; i < 4; i++) {
-        if (level == tys[i]) {
+
+    int     i;
+    int     choice;
+    
+    i      = -1;
+    choice = -1;
+    std::string tys[4] = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR"
+    };
+    void ( Harl::*com_s[4] )( void ) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+    while (++i < 4)
+    {
+        if (level == tys[i])
+        {
             choice = i;
             break ;
         }
     }
+
     switch (choice) {
         case 0:
             (this->*com_s[0])();
