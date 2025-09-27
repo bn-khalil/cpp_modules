ccf462dbcf086c166d8ef1a9903f69727b79ac4b
bn-khalil <benkhaliltouda3201@gmail.com>
2025-09-28 00:58:52 +0100

    implement casting method

--- day06/ex00/ScalarConverter.cpp
+++ day06/ex00/ScalarConverter.cpp
@@ -1,4 +1,7 @@
 #include "ScalarConverter.hpp"
+#include <string>
+#include <iomanip>
+
 
 ScalarConverter::ScalarConverter( void ) {
     std::cout << "ScalarConverter Default constructor called!" << std::endl;
@@ -19,35 +22,36 @@ ScalarConverter::~ScalarConverter( void ) {
     std::cout << "ScalarConverter Destructor called!" << std::endl;
 }
 
-bool ScalarConverter::isPseudo(std::string str) {
-    if (str == "nan" || str == "nanf" || str == "+inf" || str == "-inf" || str == "+inff" || str == "-inff")
+bool ScalarConverter::isPseudo(std::string arg) {
+    if (arg == "nan" || arg == "nanf" || arg == "+inf" || arg == "-inf" || arg == "+inff" || arg == "-inff")
         return true;
     return false;
 }
 
-std::string ScalarConverter::trimSpaces(std::string str) {
+std::string ScalarConverter::trimSpaces(std::string arg) {
     size_t start = 0;
-    size_t end = str.length();
+    size_t end = arg.length();
 
-    while (start < str.length() && (str[start] == ' ' || str[start] == '\t' || str[start] == '\n'))
+    while (start < arg.length() && (arg[start] == ' ' || arg[start] == '\t' || arg[start] == '\n'))
         start++;
-    while (end > start && (str[end] == ' ' || str[end] == '\t' || str[end] == '\n'))
+    while (end > start && (arg[end] == ' ' || arg[end] == '\t' || arg[end] == '\n'))
         end--;
-    return str.substr(start, end - start);
+    return arg.substr(start, end - start);
 }
 
-bool ScalarConverter::isChar(std::string str) {
-    if (str[i] >= 32 && str[i] < 127)
+bool ScalarConverter::isChar(std::string arg) {
+    if (arg[0] >= 32 && arg[0] <= 126)
+        return true;
     return false;
 }
 
-void ScalarConverter::displayPseudoLiterals(std::string str) {
+void ScalarConverter::displayPseudoLiterals(std::string arg) {
     std::cout << "char: " << "impossible" << std::endl;
     std::cout << "int: " << "impossible" << std::endl;
-    if (str == "-inf" || str == "-inff") {
+    if (arg == "-inf" || arg == "-inff") {
         std::cout << "float: " << "-inf" << std::endl;
         std::cout << "double: " << "-inff" << std::endl;
-    } else if (str == "+inf" || str == "+inff") {
+    } else if (arg == "+inf" || arg == "+inff") {
         std::cout << "float: " << "+inf" << std::endl;
         std::cout << "double: " << "+inff" << std::endl;
     }
@@ -57,15 +61,123 @@ void ScalarConverter::displayPseudoLiterals(std::string str) {
     }
 }
 
+void ScalarConverter::printChar(std::string arg) {
+    if (isChar(arg))
+        std::cout << "char: '" << arg[0] << "'"<< std::endl;
+    else
+        std::cout << "char: impossible"<< std::endl;
+    std::cout << "int: " << static_cast<int>(arg[0]) << std::endl;
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "float: " << static_cast<float>(arg[0]) << "f"<< std::endl;
+    std::cout << "double: " << static_cast<double>(arg[0]) << std::endl;
+}
 
+bool ScalarConverter::isNum(std::string arg) {
+    size_t i = 0;
+    if (arg[i] == '-')
+        i++;
+    while(i < arg.length() && std::isdigit(arg[i]))
+        i++;
+    if (arg.length() == i)
+        return true;
+    return false;
+}
 
-void ScalarConverter::convert( std::string str ) {
+void ScalarConverter::displayInterger(int num) {
+    if (num >= 32 && num <= 126)
+        std::cout << "char: '" << static_cast<char>(num) << "'"<< std::endl;
+    else
+        std::cout << "char: impossible"<< std::endl;
+    std::cout << "int: " << num << std::endl;
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "float: " << static_cast<float>(num) << "f"<< std::endl;
+    std::cout << "double: " << static_cast<double>(num) << std::endl;
+}
 
-    if (isPseudo(str)) {
-        displayPseudoLiterals(str);
+void ScalarConverter::displayFloat(float num) {
+    if (num >= 32 && num <= 126)
+        std::cout << "char: '" << static_cast<char>(num) << "'"<< std::endl;
+    else
+        std::cout << "char: impossible"<< std::endl;
+    std::cout << "int: " << static_cast<int>(num) << std::endl;
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "float: " << num << "f"<< std::endl;
+    std::cout << "double: " << static_cast<double>(num) << std::endl;
+}
+
+void ScalarConverter::displayDouble(double num) {
+    if (num >= 32 && num <= 126)
+        std::cout << "char: '" << static_cast<char>(num) << "'"<< std::endl;
+    else
+        std::cout << "char: impossible"<< std::endl;
+    std::cout << "int: " << static_cast<int>(num) << std::endl;
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "float: " << static_cast<float>(num) << "f"<< std::endl;
+    std::cout << "double: " << num << std::endl;
+}
+
+void ScalarConverter::displayImpossible() {
+    std::cout << "char: impossible"<< std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: impossible" << std::endl;
+    std::cout << "double: impossible" << std::endl;
+}
+
+bool ScalarConverter::isFloat(std::string arg) {
+    size_t i = 0;
+    bool point = false;
+
+    if (arg[arg.length() - 1] != 'f')
+        return false;
+    while(i < arg.length() - 1) {
+        if (arg[i] == '.' && point == false) {
+            point = true;
+            i++;
+            continue;
+        }
+        if (!std::isdigit(arg[i]))
+            return false;
+        i++;
+    }
+    if (!point)
+        return false;
+    return true;
+}
+
+bool ScalarConverter::isDouble(std::string arg) {
+    size_t i = 0;
+    bool point = false;
+
+    while(i < arg.length() - 1) {
+        if (arg[i] == '.' && point == false) {
+            point = true;
+            i++;
+            continue;
+        }
+        if (!std::isdigit(arg[i]))
+            return false;
+        i++;
+    }
+    if (!point)
+        return false;
+    return true;
+}
+
+void ScalarConverter::convert( std::string arg ) {
+
+    if (isPseudo(arg)) {
+        displayPseudoLiterals(arg);
         return ;
     }
-    std::string cmd = trimSpaces(str);
-    if (cmd.length == 1 && isChar())
-
+    // std::arging arg = trimSpaces(arg);
+    if (isNum(arg))
+        displayInterger(std::stoi(arg));
+    else if (arg.size() <= 1)
+        printChar(arg);
+    else if (isFloat(arg))
+        displayFloat(std::stof(arg));
+    else if (isDouble(arg))
+        displayDouble(std::stod(arg));
+    else
+        displayImpossible();
 }
--- day06/ex00/ScalarConverter.hpp
+++ day06/ex00/ScalarConverter.hpp
@@ -7,17 +7,25 @@
 class ScalarConverter {
 
     private:
-        static bool isPseudo(std::string str);
-        static void displayPseudoLiterals(std::string str);
-        static std::string trimSpaces(std::string str);
-        static bool isChar(std::string str);
+        static bool isPseudo(std::string arg);
+        static void displayPseudoLiterals(std::string arg);
+        static std::string trimSpaces(std::string arg);
+        static bool isChar(std::string arg);
+        static void printChar(std::string arg);
+        static bool isNum(std::string arg);
+        static void displayInterger(int num);
+        static void displayFloat(float num);
+        static void displayDouble(double num);
+        static void displayImpossible();
+        static bool isFloat(std::string arg);
+        static bool isDouble(std::string arg);
     public:
         ScalarConverter();
         ScalarConverter( const ScalarConverter &other );
         ScalarConverter& operator= ( const ScalarConverter &diff );
         ~ScalarConverter();
 
-        static void convert( std::string str );
+        static void convert( std::string arg );
 };
 
 
