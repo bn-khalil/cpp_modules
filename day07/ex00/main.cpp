7c2fd408b470c3752ea53a3831da98d004d8f0a0
bn-khalil <benkhaliltouda3201@gmail.com>
2025-10-15 14:42:10 +0100

    move implementation from header file

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -1,5 +1,25 @@
 #include "whatever.hpp"
 
+template <typename T>
+void swap(T & param1, T & param2) {
+    T tmp = param1;
+    param1 = param2;
+    param2 = tmp;
+}
+
+template <typename U>
+
+U min(U & param1, U & param2) {
+    return param1 < param2 ? param1 : param2;
+}
+
+template <typename V>
+
+V max(V & param1, V & param2) {
+    return param1 > param2 ? param1 : param2;
+}
+
+
 int main( void ) {
 
     int a = 2;
--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -4,23 +4,12 @@
 #include <iostream>
 
 template <typename T>
-
-void swap(T & param1, T & param2) {
-    T tmp = param1;
-    param1 = param2;
-    param2 = tmp;
-}
+void swap(T & param1, T & param2);
 
 template <typename U>
-
-U min(U & param1, U & param2) {
-    return param1 < param2 ? param1 : param2;
-}
+U min(U & param1, U & param2);
 
 template <typename V>
-
-V max(V & param1, V & param2) {
-    return param1 > param2 ? param1 : param2;
-}
+V max(V & param1, V & param2);
 
 #endif
