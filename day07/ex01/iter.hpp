25bc7e00b4b05c9579be14383cbbeb8a0249ded4
bn-khalil <benkhaliltouda3201@gmail.com>
2025-10-18 11:40:46 +0100

    day07

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -5,8 +5,10 @@
 
 template <typename T, typename F>
 void iter(T *array, const size_t length, F funptr) {
-    for (size_t i = 0; i < length; i++)
-        funptr(array[i]);
+    if (array) {
+        for (size_t i = 0; i < length; i++)
+            funptr(array[i]);
+    }
 }
 
 template <typename T>
