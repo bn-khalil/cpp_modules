c2996b1458f5f838c25307367eb8cb84e6eff6b5
bn-khalil <benkhaliltouda3201@gmail.com>
2025-10-18 22:43:35 +0100

    day08/ex02

--- day08/ex02/MutantStack.hpp
+++ day08/ex02/MutantStack.hpp
@@ -1,28 +1,51 @@
-
 #ifndef MUTANTSTACK_H
 #define MUTANTSTACK_H
 
 #include <iostream>
 #include <stack>
 
-template <typename T, typename container = std::deque<T>>
+template <typename T, typename container = std::deque<T> >
+
 class MutantStack: public std::stack<T> {
     public:
-        typedef typename std::
-        MutantStack();
-        MutantStack( const MutantStack & other );
-        MutantStack & operator = ( MutantStack & other );
-        ~MutantStack();
+        typedef typename container::iterator iterator;
+        typedef typename container::const_iterator const_iterator;
 
-        void push( T ele );
-        void pop(size_t index);
+        MutantStack(){
+            std::cout << "MutantStack Default constructor called!" << std::endl;
+        }
+        
+        MutantStack( const MutantStack& other ) {
+            std::cout << "Copy MutantStack constructor called!" << std::endl;
+            (void) other;
+        }
+
+        MutantStack<T> & 
+        operator = ( MutantStack &other ) {
+            std::cout << "Copy MutantStack assinement operator called!" << std::endl;
+            (void) other;
+            return ( *this );
+        }
+
+        ~MutantStack() {
+            std::cout << "MutantStack Destructor called!" << std::endl;
+        }
+
+        iterator begin() {
+            return this->c.begin();
+        }
+
+        iterator end() {
+            return this->c.end();
+        }
+
+        const_iterator begin() const {
+            return this->c.begin();
+        }
+
+        const_iterator end() const {
+            return this->c.end();
+        }
 };
-
-// template <typename T>
-// void operator ++ ( MutantStack<T> & other );
-// template <typename T>
-// void operator -- ( MutantStack<T> & other );
-// template <typename T>
-// MutantStack<T> & operator * ( MutantStack<T> & other );
 
 #endif
--- day08/ex02/main.cpp
+++ day08/ex02/main.cpp
@@ -1,78 +1,28 @@
 #include "MutantStack.hpp"
 
-template <typename T>
-MutantStack<T>::MutantStack(){
-    std::cout << "MutantStack Default constructor called!" << std::endl;
-}
-
-template <typename T>
-MutantStack<T>::MutantStack( const MutantStack& other ):sk(other.sk) {
-    std::cout << "Copy MutantStack constructor called!" << std::endl;
-}
-
-template <typename T>
-MutantStack<T> & MutantStack<T>::operator = ( MutantStack &other ) {
-    std::cout << "Copy MutantStack assinement operator called!" << std::endl;
-    if (this != &other)
-        this->sk = other.sk;
-    return ( *this );
-}
-
-template <typename T>
-MutantStack<T>::~MutantStack() {
-    std::cout << "MutantStack Destructor called!" << std::endl;
-}
-
-template <typename T>
-void MutantStack<T>::push( T ele ) {
-    this->sk.push(ele);
-}
-
-template <typename T>
-void MutantStack<T>::pop(size_t index) {
-    this->sk.pop(index);
-}
-
-template <typename T>
-void operator ++ ( MutantStack<T> & other ) {
-    other.sk++;
-}
-
-template <typename T>
-void operator -- ( MutantStack<T> & other ) {
-    other.sk--;
-}
-
-template <typename T>
-MutantStack<T> & operator * ( MutantStack<T> & other ) {
-    return other.sk;
-}
-
 int main()
 {
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << *mstack << std::endl;
-
-    // std::cout << mstack.top() << std::endl;
-    // mstack.pop();
-    // std::cout << mstack.size() << std::endl;
-    // mstack.push(3);
-    // mstack.push(5);
-    // mstack.push(737);
-    // //[...]
-    // mstack.push(0);
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    // ++it;
-    // --it;
-    // while (it != ite)
-    // {
-    // std::cout << *it << std::endl;
-    // ++it;
-    // }
-    // std::stack<int> s(mstack);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
 
     return 0;
 }
